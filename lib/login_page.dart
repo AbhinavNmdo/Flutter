6b5c99985952b743382e80bef2b2afdd7508d21c
AbhinavNmdo <abhaynam22@gmail.com>
2022-06-20 17:28:48 +0530

    Make login page learn ElevatedButton, SizedBox, TextFormFeild, Padding, GoogleFont, Image

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -6,13 +6,56 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text('Login Page', style: TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-          color: Colors.blue,
-        ),),
-      ),
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset("assets/images/login.png", fit: BoxFit.cover),
+          const SizedBox(
+            height: 20.0,
+          ),
+          const Text("Welcome", style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.deepPurpleAccent,
+          )),
+          const SizedBox(
+            height: 20.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 32),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: "Enter Username",
+                    labelText: "Username",
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    hintText: "Enter Password",
+                    labelText: "Password",
+                  ),
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    print('Hi Abhay');
+                  },
+                  child: Text('Login'),
+                  style: TextButton.styleFrom(),
+                ),
+              ],
+            )
+          )
+        ],
+      )
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import "package:flutter/material.dart";
 import 'package:flutter_catelog/login_page.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 import 'home_page.dart';
 
 void main() {
@@ -11,12 +11,15 @@ class myApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.blue),
+      themeMode: ThemeMode.system,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryTextTheme: GoogleFonts.latoTextTheme(),
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
-      initialRoute: '/home',
       routes: {
         "/" : (context) => LoginPage(),
         "/login" : (context) => LoginPage(),
